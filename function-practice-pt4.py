55857601779293149580105d8446ea3c01b15586
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-19 21:58:04 -0700

    finished third function where I swap values rather than just pushing the last value to the first index of a new arr/string

--- function-practice-pt4.py
+++ function-practice-pt4.py
@@ -12,4 +12,15 @@ def mult_list(lst):
     print(product)
 
 
-mult_list([2, 3, 5, 6])
+# mult_list([2, 3, 5, 6])
+
+def rev_string(s):
+    s = list(s)
+    for i in range(len(s)//2):
+        temp = s[i]
+        s[i] = s[-i-1]
+        s[-i-1] = temp
+    print(''.join(s))
+
+
+rev_string("yooo")
