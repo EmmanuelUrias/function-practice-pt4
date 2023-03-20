a60250859384a9614a5beea7f5af5fcce59cb09d
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-19 21:34:38 -0700

    second function complete

--- function-practice-pt4.py
+++ function-practice-pt4.py
@@ -3,4 +3,13 @@ def max_num(num1, num2, num3):
     return answer
 
 
-print(max_num(1, 4, 1))
+# print(max_num(1, 4, 1))
+
+def mult_list(lst):
+    product = lst[0]
+    for i in range(1, len(lst)):
+        product *= lst[i]
+    print(product)
+
+
+mult_list([2, 3, 5, 6])
